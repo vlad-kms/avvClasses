06af31e425fe0a9a2c3845eae1ad66b53ee5a719
Vladimir Alexeev <9141778236@mail.ru>
2023-12-07 23:33:21 +1000

    Закончил разработку avvBase.clone() - клонирование объекта

---
+++ examples/clone-avvBase.ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(ValueFromPipeline=$True, Position=0)]
+    [string] $ini="E:\!my-configs\configs\src\dns-hostinger\dns-cli.ps1.ini"
+)
+
+Get-InfoModule
+Import-Module ( ((Get-InfoModule).pathMain | Split-Path -Parent)|Join-Path -ChildPath 'classes\avvBase.ps1' ) -Force -ErrorAction Stop
+#Import-Module ( ((Get-InfoModule).pathMain | Split-Path -Parent)|Join-Path -ChildPath 'classes\classCFG.ps1' ) -Force -ErrorAction Stop
+
+. .\classes\classCFG.ps1
+
+$global:i = [iniCFG]::new($ini)
