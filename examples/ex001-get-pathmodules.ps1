fec13175ae77c96f3cd96b774897bdd9615d1a67
Vladimir Alexeev <9141778236@mail.ru>
2024-02-03 07:04:02 +1000

    004     Продолжить документирование

---
+++ examples/ex001-get-pathmodules.ps1
@@ -0,0 +1,7 @@
+import-module D:\Tools\~scripts.ps\avvClasses\avvClasses
+#. D:\Tools\~scripts.ps\avvClasses\classes\avvBase.ps1
+#. D:\Tools\~scripts.ps\avvClasses\classes\classCFG.ps1
+
+#Get-PathModules
+$p=(Get-PathModules)
+$p
